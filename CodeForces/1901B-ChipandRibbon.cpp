d696d73a097e01e7e14569ec72f9a706e03d5395
Prajwal-Vijay <prajvi.10@gmail.com>
2023-12-28 13:38:28 +0530

    Codeforces

--- CodeForces/1901B-ChipandRibbon.cpp
+++ CodeForces/1901B-ChipandRibbon.cpp
@@ -10,5 +10,20 @@ int main(){
         cases--;
         int n;
         cin >> n;
+        int c[n+1];
+        for(int i = 0; i < n; i ++){
+            cin >> c[i];
+        }
+        c[n] = 1;
+        long long int teleports = 0;
+        for(int i = 0; i < n; i++){
+            if(c[i+1] < c[i]){
+            teleports += (long long int) (c[i]-c[i+1]);
+            }
+            if(i == n-1 && c[n-1] == 0){
+                teleports += (c[i] - c[i+1]);
+            }
+        }
+        cout << teleports << endl;
     }
 }
